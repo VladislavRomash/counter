bd630a2aefb424c3d692fec59abbec819d650aab
VladislavRomash <romashkanvlad@gmail.com>
2022-04-28 18:22:55 +0300

    Created new app 'Counter'

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+export const App = () => {
+    return (
+        <div className="App">
+            <div>header</div>
+            <div>count</div>
+            <div>
+                <button>reset</button>
+                <button>add +1</button>
+                <button>reduce -1</button>
+            </div>
+
+
+        </div>
+    )
+}
